1f56c191135ce964feb8c37d06730f233218ef10
payal-dhanwate08 <dhanwatepayal@gmail.com>
2025-11-03 22:21:03 +0530

    Add files via upload

---
+++ Assignment/Assignment7/program1_5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void MultipleDisplay(int iNo)
+{
+    int iCnt = 0;
+    int iMult = 0;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    for(iCnt = 1; iCnt<=5 ; iCnt++)
+    {
+           iMult = iNo*iCnt;
+           printf("%d\t",iMult);
+        
+    }
+}
+int main()
+{
+    int iValue = 0;
+    printf("Enter number");
+    scanf("%d",&iValue);
+    MultipleDisplay(iValue);
+
+    return 0 ;
+}
