18dc52f3fdce813a3e9dadb730723b5397de2442
Payal Dhanwate <dhanwatepayal@gmail.com>
2025-11-23 19:46:15 +0530

    Add files via upload

---
+++ Assignment/Assignment21/program1_3.c
@@ -0,0 +1,40 @@
+// input :  iRow = 4 , icol = 3
+// output : 
+/*
+   5 4 3 2  1
+   5 4 3 2  1
+   5 4 3 2  1
+   
+*/
+
+#include<stdio.h>
+void Pattern(int iRow,int iCol)
+{
+    int i = 0,j = 0;
+
+    for(i = iRow; i>= 1; i--)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            printf("%d\t",j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    printf("Enter number of Rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns :\n");
+    scanf("%d",&iValue2);
+
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
